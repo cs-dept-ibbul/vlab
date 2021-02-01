baf503e4f638849eb4ba8fc12df5876253422d20
Abdullahi Abubakar Kawu <abdullahibnkawu@gmail.com>
2021-02-01 15:41:15 +0100

    Update template.blade.php

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -6,5 +6,5 @@
 @endsection
 
 <!-- use this it will give you animate play icon  on embeded video-->
- <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="venobox play-btn mb-4" data-vbtype="video" data-autoplay="true"></a>
+ <a href="https://www.youtube.com/watch?v=EJ3f_nve9EE" class="venobox play-btn mb-4" data-vbtype="video" data-autoplay="true"></a>
  
