cc82497dac453ee17d6d0d62afc4f694fc366cc2
Said Abdulsalam <42839808+saidabdul80@users.noreply.github.com>
2021-06-03 20:44:21 +0100

    fix:update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength(255);
+        Schema::defaultStringLength(191);
     }
 }
